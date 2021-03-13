7e5bdd704187408fc1c0a951ac0ddee0963cb0e5
argad <argad9292@gmail.com>
2021-03-13 14:17:24 +0300

    20210311 - homework 5 learning setup

--- module_5/test_product_page.py
+++ module_5/test_product_page.py
@@ -1,6 +1,7 @@
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
+import time
 import pytest
 
 link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo={}"
@@ -81,3 +82,35 @@ class TestProductPage:
         page.go_to_basket_page()
         basket_page = BasketPage(browser, browser.current_url)
         basket_page.should_be_empty_basket()
+
+    @pytest.mark.testclass
+    class TestUserAddToBasketFromProductPage:
+        @pytest.fixture(scope="function", autouse=True)
+        def setup(self, browser):
+            page = ProductPage(browser, link)
+            page.open()
+            page.go_to_login_page()
+            login_page = LoginPage(browser, browser.current_url)
+            email = str(time.time()) + "@fakemail.org"
+            login_page.register_new_user(email=email, password=email)
+            login_page.should_be_authorized_user()
+
+        def test_user_cant_see_success_message(self, browser):
+            # Arrange
+            page = ProductPage(browser, link.format(default_promo_offer))
+            page.open()
+            # Assert
+            page.should_not_be_success_message()
+
+        def test_user_can_add_product_to_basket(self, browser):
+            # Arrange
+            page = ProductPage(browser, link.format(default_promo_offer))
+            page.open()
+            page.get_product_data()
+            # Act
+            page.add_product_to_basket()
+            # Assert
+            page.should_be_success_message()
+            page.should_be_correct_product_in_success_message()
+            page.should_be_basket()
+            page.should_be_correct_price_in_basket()
